d080bd5f1c3a83cd9839b3dc5bd47c62923c2506
Дмитрий Калашник <dimon9b@ukr.net>
2018-12-02 18:14:01 +0200

    Add dropdown for filter receipts

--- src/app/component/recipes/components/recipes-list/recipes-list.component.ts
+++ src/app/component/recipes/components/recipes-list/recipes-list.component.ts
@@ -3,6 +3,7 @@ import { RecipesService } from '../../../../service/recipes.service';
 import { Recipe } from '../../../../model/recipe';
 import { ProductService } from '../../../../service/product.service';
 import { Product } from '../../../../model/product';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-recipes-list',
@@ -10,29 +11,58 @@ import { Product } from '../../../../model/product';
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent implements OnInit {
+  public userProductList: Product[];
+  public allRecipeList: Recipe[];
+  public filteredRecipeList: Recipe[];
 
-  public recipesList: Recipe[];
-  public productList: Array<Product>;
-  public recipeProductList: Array<Product>;
+  public productMatch = 100;
+  public sortedRecipeList: RecipeMatchItem[] = [];
 
   constructor(private recipesService: RecipesService,
     private productService: ProductService) { }
 
   ngOnInit() {
     this.productService.getProductList().subscribe(productList => {
-      this.productList = productList;
+      this.userProductList = productList;
+    });
+    this.recipesService.getRecipes().subscribe(recipes => {
+      this.allRecipeList = recipes;
     });
   }
 
   public showRecipes() {
-    this.recipesService.getRecipes().subscribe(recipesList => {
-      this.recipesList = recipesList.filter(receipt => {
-        const receiptProductList = receipt.productList;
-        const foundedProducts = receiptProductList.filter(productInReceipt => {
-          return this.productList.find(productInFridge => productInFridge.name === productInReceipt.name);
-        });
-        return foundedProducts.length === receiptProductList.length;
+    this.sortProductList();
+    this.filterRecipeListByProductMatch(this.productMatch);
+  }
+
+  filterRecipeListByProductMatch(productMatch: number) {
+    let lastIndex = this.sortedRecipeList.length;
+    if (productMatch > 0) {
+      const foundIndex = this.sortedRecipeList.findIndex(p => p.productMatch < productMatch);
+      lastIndex = foundIndex > -1 ? foundIndex : lastIndex;
+    }
+    this.filteredRecipeList = this.sortedRecipeList.slice(0, lastIndex).map(recipeMatch => recipeMatch.recipe);
+  }
+
+  private sortProductList() {
+    this.allRecipeList.forEach(recipe => {
+      const receiptProductList = recipe.productList;
+      const foundedProducts = receiptProductList.filter(productInReceipt => {
+        return this.userProductList.find(productInFridge => productInFridge.name === productInReceipt.name);
       });
+      const productMatch = (foundedProducts.length / recipe.productList.length) * 100;
+      this.sortedRecipeList.push(new RecipeMatchItem(recipe, productMatch));
     });
+    this.sortedRecipeList = this.sortedRecipeList.sort((r1, r2) => r2.productMatch - r1.productMatch);
+  }
+}
+
+class RecipeMatchItem {
+  recipe: Recipe;
+  productMatch: number;
+
+  constructor(recipe: Recipe, productMatch: number) {
+    this.recipe = recipe;
+    this.productMatch = productMatch;
   }
 }
--- src/app/service/recipes.service.ts
+++ src/app/service/recipes.service.ts
@@ -1,21 +1,17 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import {Recipe} from "../model/recipe";
-import {Observable, of} from "rxjs";
-import {Product} from "../model/product";
+import { HttpClient } from '@angular/common/http';
+import { Recipe } from '../model/recipe';
+import { Observable } from 'rxjs';
+import { Product } from '../model/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesService {
 
-  productList: Array<Product> = [];
-  allRecipes: Array<Recipe> = [];
-
-  constructor(private http: HttpClient) {  }
+  constructor(private http: HttpClient) { }
 
   public getRecipes(): Observable<Recipe[]> {
     return this.http.get<Recipe[]>('http://localhost:3000/recipes');
   }
-
 }
